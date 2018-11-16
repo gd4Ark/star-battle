5c490615ee31f13e5fdd3fca20b5aee96d8dc113
qywuyanzu <1806804351@qq.com>
2018-11-16 09:36:14 +0800

    Delete promotion area module

--- js/main.js
+++ js/main.js
@@ -2,15 +2,3 @@ res.loadAssets(() => {
     const game = new Game();
     game.start();
 });
-
-on(window,'load',()=>{
-    setTimeout(()=>{
-        on(
-            $('video'),
-            'mouseover',
-            ()=>{
-                $('video').play();
-            }
-        )
-    },50);
-});
