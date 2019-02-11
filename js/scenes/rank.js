f69acf0ec7801b1524d2e0773ac478ab3a6660b7
gd4ark <gd.4ark@gmail.com>
2019-02-12 00:56:35 +0800

    修正排行榜排序逻辑

--- js/scenes/rank.js
+++ js/scenes/rank.js
@@ -1,4 +1,4 @@
-class Rank extends Scene{
+class Rank extends Scene {
     setup() {
         super.setup();
         this.event();
@@ -7,57 +7,59 @@ class Rank extends Scene{
         this.rank();
     }
 
-    addData(){
+    addData() {
         const key = this.storageDataKey;
         const {
             name,
             score,
             time,
         } = this.game.data;
-        localStorageData.add(key,{
+        localStorageData.add(key, {
             name,
             score,
             time,
         });
     }
 
-    rank(){
-       const key = this.storageDataKey;  
-       let html = ""; 
-       let position = 0; 
-       let data = [].concat(localStorageData.get(key).data);
-       const some = (a,b)=>{
-           return (
+    rank() {
+        const key = this.storageDataKey;
+        let html = "";
+        let position = 0;
+        let data = [].concat(localStorageData.get(key).data);
+        const some = (a, b) => {
+            return (
                 (a.score === b.score) && (a.time === b.time)
-           );
-       }
-       data.sort((a,b)=>{
-           if (a.score === b.score){
-               return a.time < b.time ? 1 : -1;
-           }
-           return a.score < b.score ? 1 : -1;
-       });
-       localStorageData.update(key,data);
-       data.map((el,index)=>{
-            const prev = data[index-1];
-            if (prev){
-                position += some(prev,el) ? 0 : 1; 
-            }else{
-                position++;
+            );
+        }
+        data.sort((a, b) => {
+            if (a.score === b.score) {
+                return a.time < b.time ? 1 : -1;
             }
-            html += `
-                <tr>
-                    <td>${numberFormat(position)}</td>
-                    <td>${el.name}</td>
-                    <td>${numberFormat(el.score)}</td>
-                    <td>${numberFormat(el.time)}</td>
-                </tr>
-            `;
-       });
-       $('#rank table tbody').innerHTML = html;
+            return a.score < b.score ? 1 : -1;
+        });
+        setTimeout(() => {
+            localStorageData.update(key, data);
+            data.map((el, index) => {
+                const prev = data[index - 1];
+                if (prev) {
+                    position += some(prev, el) ? 0 : 1;
+                } else {
+                    position++;
+                }
+                html += `
+                 <tr>
+                     <td>${numberFormat(position)}</td>
+                     <td>${el.name}</td>
+                     <td>${numberFormat(el.score)}</td>
+                     <td>${numberFormat(el.time)}</td>
+                 </tr>
+             `;
+            });
+            $('#rank table tbody').innerHTML = html;
+        }, 0);
     }
 
-    event(){
+    event() {
         on(
             $('#restart-btn'),
             'click',
